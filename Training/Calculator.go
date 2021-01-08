ed28c9107decf384a42fb77a0b8acdb8a971d8b2
vysakhsm <77090495+vysakhsm@users.noreply.github.com>
2021-01-08 11:12:55 +0530

    Update Calculator.go

--- Training/Calculator.go
+++ Training/Calculator.go
@@ -10,8 +10,8 @@ func main(){
 	http.ListenAndServe(":8080",nil)
 }
 func Calculate(w http.ResponseWriter, r *http.Request){
-	var number1 string
-	var number2 string
+	var string1 string
+	var string2 string
 	//var number3 int
 	//var number4 int
 	if r.URL.Path != "/"{
@@ -21,17 +21,17 @@ func Calculate(w http.ResponseWriter, r *http.Request){
 	switch r.Method{
 	case "GET": 
 		http.ServeFile(w, r, "AddPage.html")
-		fmt.Fprintf(w,"This is calculator")
+		fmt.Fprintf(w,"This is String")
 	case "POST":
-		fmt.Fprintf(w,"This is calculator after POST\n ")
-		number1 = r.FormValue("number1")
-		number2 = r.FormValue("number2")
-		fmt.Fprintf(w, "Number 1 = %v\n", number1)
-		fmt.Fprintf(w, "Number 2 = %v\n", number2)
+		fmt.Fprintf(w,"This is after POST\n ")
+		string1 = r.FormValue("string1")
+		string2 = r.FormValue("string2")
+		fmt.Fprintf(w, "String 1 = %v\n", string1)
+		fmt.Fprintf(w, "String 2 = %v\n", string2)
 		//if number3, err := strconv.ParseInt(number1,6,12); err != nil{
 		//	fmt.Fprintf(w,"Error occured ")
 		//}
 		//number4 = number3
-		fmt.Fprintf(w, "Sum  = %v \n", number1 + number2)
+		fmt.Fprintf(w, "Appended String  = %v \n", string1 + string2)
 	}
 }
